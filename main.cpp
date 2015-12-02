235140c70b5f09c0d264d595f27737474bdda5bb
Matt Hamil <j.matthew.hamil@gmail.com>
2015-12-01 19:37:25 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// James Hamil
+// w812971
+
+#include <iostream>
+#include "bt_class.h"
+using main_savitch_10::binary_tree;
+using namespace std;
+
+int main() {
+    binary_tree<int> *test;
+    
+
+    return 0;
+}
